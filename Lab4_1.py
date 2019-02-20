0e5856d6c5b23058f8c4d98c3b6b81b080bcaa24
Nathan W <nathan@email.com>
2019-02-19 21:01:24 -0600

    Added variables as input for user to insert a new record holder

--- Lab4_1.py
+++ Lab4_1.py
@@ -6,4 +6,9 @@ cur = db.cursor()
 
 cur.execute('create table if not exists Record_Holders (holder name, holder country, catches integor')
 
+holder = input('Enter Record Holder\'s Full Name: ')
+country = input('Enter Record Holder\'s Country: ')
+catches = int(input('Enter their total number of catches: '))
+
 cur.execute('insert into Record_Holders values (?, ?, ?)', (holder, country, catches))
+
