9d1a2f293d9fe727ab1a601da25a887ccd4d137a
Nathan W <nathan@email.com>
2019-02-20 08:32:56 -0600

    Added search for holders name.

--- Lab4_1.py
+++ Lab4_1.py
@@ -4,11 +4,18 @@ db = sqlite3.connect('chainsaw_juggle_records.db')
 
 cur = db.cursor()
 
-cur.execute('create table if not exists Record_Holders (holder name, holder country, catches integor')
+cur.execute('create table if not exists Record_Holders (holder, country, catches)')
 
 holder = input('Enter Record Holder\'s Full Name: ')
 country = input('Enter Record Holder\'s Country: ')
 catches = int(input('Enter their total number of catches: '))
 
 cur.execute('insert into Record_Holders values (?, ?, ?)', (holder, country, catches))
+
+search_param = input('Enter Record Holder\'s Full Name you Would Like to Search: ')
+
+for row in cur.execute('SELECT rowid, * FROM Record_Holders WHERE holder = ?', (search_param, id)):
+    print(row['holder'])
+    print(row['country'])
+    print(row['catches'])
 
